86deb5460ddf61d0e53b6221f965c8ed5a88a85b
achal bi <achal.rvce@gmail.com>
2016-07-25 16:29:51 +0530

    set initial state url

--- app/serializers/workflow_serializer.rb
+++ app/serializers/workflow_serializer.rb
@@ -1,4 +1,9 @@
 class WorkflowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
+  attributes :id, :name, :description, :initial_state_url
   has_many :workflow_states
+
+  def initial_state_url
+  	# workflow_workflow_state_path(object, object.workflow_states[0]);
+  	"https://middle-men.herokuapp.com/workflows/"+object.id+"/workflow_states/"+object.workflow_states[0].id
+  end
 end
